5210446b9f624422982db660569f6a8d61dd3566
Luc Vanlerberghe <luc.vanlerberghe@bvdinfo.com>
2017-01-11 09:03:35 +0100

    SimpleProducer: Changed to single-message version and extracted ProduceAndReportUsingContinueWithAsync method

--- examples/SimpleProducer/Program.cs
+++ examples/SimpleProducer/Program.cs
@@ -9,25 +9,47 @@ namespace SimpleProducer
     {
         public static void Main(string[] args)
         {
-            string brokerList = args[0];
-            string topicName = args[1];
+            string brokerList = "127.0.0.1:9092"; // args[0];
+            string topicName = "testtopic"; // args[1];
 
-            using (Producer producer = new Producer(brokerList))
-            using (Topic topic = producer.Topic(topicName))
+            var config = new Config();
+
+            config["debug"] = "all";
+
+            using (Producer producer = new Producer(config, brokerList))
             {
-                Console.WriteLine($"{producer.Name} producing on {topic.Name}. q to exit.");
+                producer.OnError += Producer_OnError;
+                producer.OnStatistics += Producer_OnStatistics;
 
-                string text;
-                while ((text = Console.ReadLine()) != "q")
+                using (Topic topic = producer.Topic(topicName))
                 {
-                    byte[] data = Encoding.UTF8.GetBytes(text);
-                    Task<DeliveryReport> deliveryReport = topic.Produce(data);
-                    var unused = deliveryReport.ContinueWith(task =>
-                    {
-                        Console.WriteLine($"Partition: {task.Result.Partition}, Offset: {task.Result.Offset}");
-                    });
+                    string text = "test";
+
+                    var task = ProduceAndReportUsingContinueWithAsync(topic, text);
+
+                    task.Wait();
                 }
             }
+        }
+
+        private static void Producer_OnStatistics(object sender, string e)
+        {
+            Console.WriteLine($"OnStatistics: {e}");
+        }
+
+        private static void Producer_OnError(object sender, Handle.ErrorArgs e)
+        {
+            Console.WriteLine($"OnError: {e.ErrorCode}: {e.Reason}");
+        }
+
+        private static Task ProduceAndReportUsingContinueWithAsync(Topic topic, string text)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(text);
+            Task<DeliveryReport> deliveryReport = topic.Produce(data);
+            return deliveryReport.ContinueWith(task =>
+            {
+                Console.WriteLine($"Partition: {task.Result.Partition}, Offset: {task.Result.Offset}");
+            });
         }
     }
 }
