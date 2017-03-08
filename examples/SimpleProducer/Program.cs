9949e54d1664277d202e6cc95dc8b4531378bc4e
Luc Vanlerberghe <luc.vanlerberghe@bvdinfo.com>
2017-03-08 17:20:59 +0100

    SimpleProducer: Changed to single-message version

--- examples/SimpleProducer/Program.cs
+++ examples/SimpleProducer/Program.cs
@@ -29,29 +29,42 @@ namespace Confluent.Kafka.Examples.SimpleProducer
     {
         public static void Main(string[] args)
         {
-            string brokerList = args[0];
-            string topicName = args[1];
+            string brokerList = "127.0.0.1:9092"; // args[0];
+            string topicName = "testtopic"; // args[1];
 
-            var config = new Dictionary<string, object> { { "bootstrap.servers", brokerList } };
+            var config = new Dictionary<string, object>
+            {
+                { "bootstrap.servers", brokerList },
+            };
 
             using (var producer = new Producer<Null, string>(config, null, new StringSerializer(Encoding.UTF8)))
             {
+                producer.OnError += Producer_OnError;
+                producer.OnStatistics += Producer_OnStatistics;
+
                 Console.WriteLine($"{producer.Name} producing on {topicName}. q to exit.");
 
-                string text;
-                while ((text = Console.ReadLine()) != "q")
+                string text = $"test: {DateTime.Now.ToString("o")}" ;
+
+                var deliveryReport = producer.ProduceAsync(topicName, null, text);
+                deliveryReport.ContinueWith(task =>
                 {
-                    var deliveryReport = producer.ProduceAsync(topicName, null, text);
-                    deliveryReport.ContinueWith(task =>
-                    {
-                        Console.WriteLine($"Partition: {task.Result.Partition}, Offset: {task.Result.Offset}");
-                    });
-                }
+                    Console.WriteLine($"Partition: {task.Result.Partition}, Offset: {task.Result.Offset}");
+                });
 
                 // Tasks are not waited on synchronously (ContinueWith is not synchronous),
                 // so it's possible they may still in progress here.
                 producer.Flush();
             }
+        }
+        private static void Producer_OnStatistics(object sender, string e)
+        {
+            Console.WriteLine($"OnStatistics: {e}");
+        }
+
+        private static void Producer_OnError(object sender, Error error)
+        {
+            Console.WriteLine($"OnError: {error.Code} ({error.Code.GetReason()}): {error.Reason}");
         }
     }
 }
